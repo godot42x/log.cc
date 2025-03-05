7ad7980330575420fac7ec120f6eaf9f0cac09ed
godot42 <disyourself@qq.com>
2025-03-05 17:31:14 +0800

    rearrange codes

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,7 @@
-
 --print("Check if did not add 'fmt' packages in your toplevel xmake.lua")
-add_requires("fmt")
-add_packages("fmt")
 
-target("m_log")
+target("log.cc")
+do
     set_kind("shared")
 
     if is_os("linux") then
@@ -12,24 +10,25 @@ target("m_log")
         add_defines("BUILD_SHARED_M_LOG")
     end
 
+    set_languages("c++20")
+
     add_headerfiles("*.h")
     add_files("log.cpp")
+    add_includedirs(".")
 
-    add_packages("fmt")
     on_config(function(target)
-
         if is_host("window") then
             -- Check if the /Zc:preprocessor flag is not added
             if not target:has_cxxflags("/Zc:preprocessor") then
                 print(
-					'\27[31m'..
+                    '\27[31m' ..
                     '[NOTICE] Add these to top-level "xmake.lua" if you are using MSVC:' ..
                     '\n\tif is_plat("windows") then ' ..
                     '\n\t    add_cxflags("/Zc:preprocessor")' ..
-                    '\n\tend'..
-					'\27[Om'
+                    '\n\tend' ..
+                    '\27[Om'
                 )
             end
         end
     end)
-
+end
